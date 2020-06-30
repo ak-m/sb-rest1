67acbe5aefdacbd4c94852a0783236d03701fe60
ak-m <ak.ghub.m@gmail.com>
2020-06-30 15:57:59 -0400

    Initial commit spring boot app

---
+++ src/main/java/com/sts/projs/sbrest1/StsSbRest1Application.java
@@ -0,0 +1,13 @@
+package com.sts.projs.sbrest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsSbRest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsSbRest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sts/projs/StsSbRest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sts.projs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsSbRest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
